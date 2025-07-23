e00836dee16238d2a9262701a7f7c02bca887247
sandeshkc <sandeshkhatri007@gmail.com>
2025-07-23 23:08:03 +0545

    initial commit

---
+++ frontend/src/components/navBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav className='w-screen'>
+        <h1>Sandesh</h1>
+        <div>
+            
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ frontend/src/pages/LandingPage/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Info from '../../components/landingPage/Info'
+import Nav from '../../components/navBar'
+function Home() {
+  return (
+    <div>
+      <Info />
+      <Nav/>
+    </div>
+  )
+}
+
+export default Home
